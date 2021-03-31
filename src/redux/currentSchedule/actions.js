e42752272ccd523590d276642ff2abc73678a869
etokohei <kohei.eto@rakus-partners.co.jp>
2021-03-31 16:29:26 +0900

    push やり直し

--- src/redux/currentSchedule/actions.js
+++ src/redux/currentSchedule/actions.js
@@ -1,9 +1,9 @@
-// constants
+//constants
 export const CURRENT_SCHEDULE_SET_ITEM = "CURRENT_SCHEDULE_SET_ITEM";
 export const CURRENT_SCHEDULE_OPEN_DIALOG = "CURRENT_SCHEDULE_OPEN_DIALOG";
 export const CURRENT_SCHEDULE_CLOSE_DIALOG = "CURRENT_SCHEDULE_CLOSE_DIALOG";
 
-// actions
+//actions
 export const currentScheduleSetItem = payload => ({
   type: CURRENT_SCHEDULE_SET_ITEM,
   payload
